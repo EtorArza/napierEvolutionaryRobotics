acee7d71a490138caab01086f7b5879660386ed7
EtorArza <etorarza@gmail.com>
2022-01-07 23:44:05 +0100

    experiment launches

---
+++ launch.sh
@@ -0,0 +1,2 @@
+cp parameters.csv /home/paran/Dropbox/BCAM/07_estancia_1/code/evorl_gecco_2021/experiments/mnipes/parameters.csv
+./evorl_gecco_2021/CoppeliaSim_Edu_V4_1_0_Ubuntu18_04/coppeliaSim.sh -h -g/home/paran/Dropbox/BCAM/07_estancia_1/code/evorl_gecco_2021/experiments/mnipes/parameters.csv
