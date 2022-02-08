f88904741ad61997ef45c9562f568b6a6c82990f
EtorArza <etorarza@gmail.com>
2022-02-08 17:32:23 +0000

    update to plot fitness over time

--- scripts/plot_fitness_over_time.py
+++ scripts/plot_fitness_over_time.py
@@ -1,11 +1,13 @@
 from utils.functionsForcsvPlots import *
 
-dir_with_all_csv = "/home/paran/Dropbox/BCAM/07_estancia_1/code/logs/nipes_2_1_19-47-12-2665-561118790"
+dir_with_all_csv = "/home/paran/Dropbox/BCAM/07_estancia_1/code/logs/nipes_5_1_10-49-44-4668-1569205384"
 neval_list, _, best_fit_list, avg_fit_list, pop_size_list = load_all_fitnesses(dir_with_all_csv + "/fitnesses.csv", 0)
 _, _, best_nov_list, avg_nov_list, _ = load_all_fitnesses(dir_with_all_csv + "/fitnesses.csv",1)
 _, eval_time_list = load_eval_time(dir_with_all_csv + "/eval_durations.csv")
 
-plt.plot(neval_list, best_fit_list)
+plt.plot(neval_list, best_fit_list, label="EnvType0")
+plt.plot(neval_list, best_nov_list, label="EnvType1")
+plt.legend()
 plt.xlabel("Evaluations")
 plt.ylabel("Objective function")
 plt.show()
