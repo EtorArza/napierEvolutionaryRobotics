f4fd69d0eeb5786829f4aefd3539be9d92997cbb
EtorArza <etorarza@gmail.com>
2022-02-10 17:49:14 +0000

    update plot fitness over time script

--- scripts/plot_fitness_over_time.py
+++ scripts/plot_fitness_over_time.py
@@ -1,6 +1,19 @@
 from utils.functionsForcsvPlots import *
 
-dir_with_all_csv = "/home/paran/Dropbox/BCAM/07_estancia_1/code/logs/nipes_5_1_10-49-44-4668-1569205384"
+fig_dir = "results/figures"
+
+# # Results from new e-mail
+# figure_name = "FINTESS_TIME_Leni_nipes_logs"
+# dir_with_all_csv = "logs/nipes_15_5_14-57-31-1814-1756362047"
+
+# # Executed with parameters from new e-mail
+# figure_name = "FINTESS_TIME_executed_by_me"
+# dir_with_all_csv = "logs/nipes_9_1_10-54-50-58-1859741572"
+
+# # Executed with OG codebase
+figure_name = "FITNESS_TIME_og_codebase"
+dir_with_all_csv = "logs/nipes_9_1_12-42-9-9928-3753403340"
+
 neval_list, _, best_fit_list, avg_fit_list, pop_size_list = load_all_fitnesses(dir_with_all_csv + "/fitnesses.csv", 0)
 _, _, best_nov_list, avg_nov_list, _ = load_all_fitnesses(dir_with_all_csv + "/fitnesses.csv",1)
 _, eval_time_list = load_eval_time(dir_with_all_csv + "/eval_durations.csv")
@@ -10,4 +23,5 @@ plt.plot(neval_list, best_nov_list, label="EnvType1")
 plt.legend()
 plt.xlabel("Evaluations")
 plt.ylabel("Objective function")
-plt.show()
+plt.title(figure_name)
+plt.savefig(fig_dir + f"/{figure_name}.pdf")
