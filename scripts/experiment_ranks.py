3be932f1d19a38bf81b0fef3901027a7fb59b9c4
EtorArza <etorarza@gmail.com>
2022-02-16 18:23:05 +0000

    add script to measure ranks

--- scripts/experiment_ranks.py
+++ scripts/experiment_ranks.py
@@ -5,14 +5,16 @@ import numpy as np
 from tqdm import tqdm as tqdm
 from matplotlib.ticker import StrMethodFormatter
 from joblib import Parallel, delayed
+import time
+import re
+from os.path import exists
 
-
-paraneter_file = "experiments/nipes/parameters.csv"
+parameter_file = "experiments/nipes/parameters.csv"
 
 parameter_text = """
 #experimentName,string,nipes
-#subexperimentName,string,standard
-#preTextInResultFile,string,test5
+#subexperimentName,string,measure_ranks
+#preTextInResultFile,string,ftest5
 #resultFile,string,result5.txt
 
 
@@ -39,11 +41,11 @@ parameter_text = """
 #instanceType,int,0
 #killWhenNotConnected,bool,0
 #shouldReopenConnections,bool,0
-#seed,int,6
+#seed,int,7
 
-#populationSize,int,4
-#maxEvalTime,float,20.0
-#maxNbrEval,int,16
+#populationSize,int,10
+#maxEvalTime,float,30.0
+#maxNbrEval,int,20
 #timeStep,float,0.1
 
 #modifyMaxEvalTime,bool,0
@@ -97,12 +99,79 @@ parameter_text = """
 
 
 
-mass_update_parameters(paraneter_file, parameter_text)
+mass_update_parameters(parameter_file, parameter_text)
 
 
-def run_with_conf_index(conf_index):
-    print(conf_index)
+
+
+def run_with_conf_index(seed):
+    time.sleep(seed*4 % 20)
+    update_parameter(parameter_file, "seed", str(seed))
+    update_parameter(parameter_file, "resultFile", f"ranks_exp_result_{seed}.txt")
+    update_parameter(parameter_file, "preTextInResultFile", f"seed_{seed}")
+    exec_res=subprocess.run(f"bash launch.sh -e=nipes",shell=True, capture_output=True)  
+
+    
+seeds = list(range(2,101))
+
+# Parallel(n_jobs=5, verbose=12)(delayed(run_with_conf_index)(i) for i in seeds)
+
+
+def distance_between_orders(order1, order2):
+
+    n = len(order1)
+    if n % 2 != 0:
+        print("ERROR, n should be even.")
+        exit(1)
+
+    top_half_indexes1 = np.argpartition(order1, -n//2)[-n//2:]
+    top_half_indexes2 = np.argpartition(order2, -n//2)[-n//2:]
+
+
+    res = 0
+    for index in top_half_indexes2:
+        if index not in top_half_indexes1:
+            res += 1
+
+    return res
 
     
 
-Parallel(n_jobs=6, verbose=12)(delayed(run_with_conf_index)(i) for i in range(1,13))
+    return int(i1 != i2)
+
+
+distances = dict()
+for i in seeds:
+    if not exists(f"logs/ranks_exp_result_{i}.txt"):
+        continue
+    with open(f"logs/ranks_exp_result_{i}.txt") as f:
+        lines = list(map(lambda x: x.strip("\n"), f.readlines()))
+        if len(lines) != 7:
+            print("Skipping line of len",len(lines))
+            continue
+        lines = lines[:-1]
+        lines = [re.split("\(|\)",el) for el in lines]
+
+        lines = [lines[i][0].strip(",").split(",") + lines[i][1:] for i in range(len(lines))]
+        for line_idx in range(len(lines)):
+            lines[line_idx][1] = float(lines[line_idx][1])
+            lines[line_idx][3] = list(map(float, lines[line_idx][3].split(",")))
+            lines[line_idx].remove(",")
+            lines[line_idx][4] = list(map(float, lines[line_idx][4].split(",")))
+        ref_order = lines[0][3]
+        for line in (lines[1:]):
+            if line[1] not in distances:
+                distances[line[1]] = []
+            distances[line[1]].append(distance_between_orders(ref_order, line[3]))
+
+
+
+x_list = []
+y_list = []
+for x,y in sorted(distances.items()):
+    x_list.append(x)
+    y_list.append(np.average(y))
+
+
+plt.plot(x_list,y_list)
+plt.show()
